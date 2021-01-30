bc7836344f5f08b6af79b89d4ed6a983874d5653
geertza <geertza@gmail.com>
2021-01-30 02:04:48 -0700

    t

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.get('/', function (req, res) {
   res.send('Hello World      yeah boi')
 })
-const port = 5000;
+const port = 3000;
 
 app.listen(port, console.log(`Server running on port ${port}`));
